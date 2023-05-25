4a8323e778d32ac235f63db0af350a691ac2bffc
fathurizkym <fathurizkym27@gmail.com>
2023-05-25 14:25:01 +0700

    nambah T

--- capstone_data_stock_gudang.py
+++ capstone_data_stock_gudang.py
@@ -105,7 +105,7 @@ def add():
                             ]
                         })
                         add()
-                    elif yakin == "T":
+                    elif yakin == "TT":
                         add()
                     else :
                         print("Input yang anda masukan salah!!! Input (Y/T) ")
